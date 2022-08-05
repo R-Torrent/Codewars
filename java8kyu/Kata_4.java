ff1e55a3a0a979368987b0eac11c94d556b74e3d
R-Torrent <rta@alumnes.upm.es>
2022-08-05 12:20:54 +0200

    Reversed Strings

---
+++ java8kyu/Kata_4.java
@@ -0,0 +1,11 @@
+package java8kyu;
+
+/** Reversed Strings
+ *  8 kyu */
+public class Kata_4 {
+	
+	public static String solution(String str) {
+		return new StringBuilder(str).reverse().toString();
+	}
+	
+}

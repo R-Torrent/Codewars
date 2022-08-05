1b289e55f05cd01f9c81138037519a5d27e49bce
R-Torrent <rta@alumnes.upm.es>
2022-08-05 13:30:37 +0200

    Switcheroo

--- java7kyu/Switcheroo.java
+++ java7kyu/Switcheroo.java
@@ -4,6 +4,10 @@ package java7kyu;
  *  7 kyu */
 public class Switcheroo {
 	
+	public static void main(String args[]) {
+		System.out.println(switcheroo("bbbacccabbb"));	
+	}
+	
 	public static String switcheroo(String x) {
 		return x.replace('a', 'x').replace('b', 'a').replace('x', 'b');
 	}

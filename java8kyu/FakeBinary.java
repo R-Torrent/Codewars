4245ee9ba2e1dd335303381a76f8c703d22e81b0
R-Torrent <rta@alumnes.upm.es>
2022-08-05 12:49:43 +0200

    Fake Binary

---
+++ java8kyu/FakeBinary.java
@@ -0,0 +1,18 @@
+package java8kyu;
+
+public class FakeBinary {
+	
+	public static void main(String args[]) {
+		System.out.println(fakeBin("45385593107843568"));
+		System.out.println(fakeBin("509321967506747"));
+		System.out.println(fakeBin("366058562030849490134388085"));
+	}
+	
+	public static String fakeBin(String numberString) {
+		return numberString.chars()
+				.map(c -> c < '5' ? '0' : '1')
+				.collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
+				.toString();
+	}
+	
+}
